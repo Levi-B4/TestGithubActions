0bfdc9141ddeb637b04fd802139897ca1ea18a83
L-Arch <levi44b@gmail.com>
2024-01-06 13:57:39 -0500

    added input average to the console and file output

--- main.cpp
+++ main.cpp
@@ -6,16 +6,29 @@ int main(int argc, char **argv) {
     std::cout << "Welcome to Day 1 - Project 0!" << std::endl;
 
     std::ifstream fin(argv[1]);
-    std::ofstream fout(argv[2]);
+    std::ofstream fout(argv[2], std::ofstream::out);
     int n;
 
+    int total = 0;
+    int numInts = 0;
+
     while(fin >> n){
-      std::cout << n << std::endl;
-      fout << n << std::endl;
+        // output integer to terminal and output file
+        std::cout << n << std::endl;
+        fout << n << std::endl;
+
+        // add integer to total and track numInts
+        total += n;
+        numInts++;
     }
 
+    // output average to terminal and output file
+    double average = total / numInts;
+    std::cout << average << std::endl;
+    fout << average << std::endl;
 
     int test = 0;
+
     fin.close();
     fout.close();
 
